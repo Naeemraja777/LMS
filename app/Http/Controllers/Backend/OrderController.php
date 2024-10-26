f91b20bc502d47c7723c608f9bc29573cdfc3e57
Naeemraja777 <muhammadnaeem.apex@gmail.com>
2024-10-25 19:11:56 -0700

    order invoice pdf

--- app/Http/Controllers/Backend/OrderController.php
+++ app/Http/Controllers/Backend/OrderController.php
@@ -16,6 +16,7 @@ use App\Models\Coupon;
 use Illuminate\Support\Facades\Session;
 use App\Models\Payment;
 use App\Models\Order;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class OrderController extends Controller
 {
@@ -57,5 +58,35 @@ class OrderController extends Controller
                                         return view('admin.backend.orders.confirm_orders',compact('payment'));
                                
                                     } // End Meth
+                                    public function InstructorAllOrder(){
+                                                $id = Auth::user()->id;
+                                                $latestOrderItem = Order::where('instructor_id',$id)->select('payment_id', \DB::raw('MAX(id) as max_id'))->groupBy('payment_id');
+
+                                                        $orderItem = Order::joinSub($latestOrderItem, 'latest_order', function($join) {
+                                            
+                                                            $join->on('orders.id', '=', 'latest_order.max_id');
+                                            
+                                                        })->orderBy('latest_order.max_id','DESC')->get();
+                                            return view('instructor.orders.all_orders',compact('orderItem'));
+                                            }// End Method
+                                            public function InstructorOrderDetails($payment_id){
+                                                        $payment = Payment::where('id',$payment_id)->first();
+                                                
+                                                        $orderItem = Order::where('payment_id',$payment_id)->orderBy('id','DESC')->get();
+                                            
+                                                        return view('instructor.orders.instructor_order_details',compact('payment','orderItem'));
+                                                
+                                                    }// End Method 
+                                                    public function InstructorOrderInvoice($payment_id){
+                                                                $payment = Payment::where('id',$payment_id)->first();
+                                                                $orderItem = Order::where('payment_id',$payment_id)->orderBy('id','DESC')->get();
+                                                                $pdf = Pdf::loadView('instructor.orders.order_pdf',compact('payment','orderItem'))->setPaper('a4')->setOption([
+                                                                    'tempDir' => public_path(),
+                                                                    'chroot' => public_path(),
+                                                                ]);
+                                                      
+                                                                return $pdf->download('invoice.pdf');
+                                                       
+                                                            }// E
 
 }

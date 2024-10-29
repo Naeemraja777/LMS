e546b0dd91256f2fd98fd737430b458874f50ca7
Naeemraja777 <muhammadnaeem.apex@gmail.com>
2024-10-29 08:38:50 +0500

    user-enrollment

--- app/Http/Controllers/Backend/OrderController.php
+++ app/Http/Controllers/Backend/OrderController.php
@@ -88,5 +88,25 @@ class OrderController extends Controller
                                                                 return $pdf->download('invoice.pdf');
                                                        
                                                             }// E
+                                                            public function MyCourse(){
+                                                          
+                                                                        $id = Auth::user()->id;   
+                                                                        $latestOrders = Order::where('user_id',$id)->select('course_id', \DB::raw('MAX(id) as max_id'))->groupBy('course_id');
+                                                              
+                                                                        $mycourse = Order::joinSub($latestOrders, 'latest_order', function($join) {
+                                                            
+                                                                            $join->on('orders.id', '=', 'latest_order.max_id');
+                                                              
+                                                                        })->orderBy('latest_order.max_id','DESC')->get();
+                                                                        return view('frontend.mycourse.my_all_course',compact('mycourse'));
+                                                              
+                                                                    }// End Method
+                                                                    public function CourseView($course_id){
+                                                                                $id = Auth::user()->id;
+                                                                                 $course = Order::where('course_id',$course_id)->where('user_id',$id)->first();
+                                                                                $section = CourseSection::where('course_id',$course_id)->orderBy('id','asc')->get();
+                                                                                return view('frontend.mycourse.course_view',compact('course','section'));
+                                                                    
+                                                                            }// End Method 
 
 }
